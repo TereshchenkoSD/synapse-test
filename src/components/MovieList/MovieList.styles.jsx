f2e12bb51121cf2374959f9b00a33f41b6c8d4db
Sergey Tereshchenko <serghh1988@gmail.com>
2021-12-16 07:37:39 +0200

    Added styles to the MovieList component and index.css.

--- src/components/MovieList/MovieList.styles.jsx
+++ src/components/MovieList/MovieList.styles.jsx
@@ -3,18 +3,34 @@ import styled from '@emotion/styled/macro';
 export const MovieGallery = styled.ul`
   display: flex;
   flex-wrap: wrap;
-  margin-left: calc(-1 * 30px);
-  margin-top: calc(-1 * 30px);
+  margin-top: 30px;
 `;
 
-export const MovieGalleryItem = styled.li``;
+export const MovieGalleryItem = styled.li`
+  margin-right: var(--card-set-gap);
+  margin-bottom: var(--card-set-gap);
+  flex-basis: calc((100% - 1 * var(--card-set-gap)) / 2);
+  width: 605px;
 
-export const MoviePoster = styled.img``;
+  &:nth-child(2n) {
+    margin-right: 0;
+  }
+`;
+
+export const MoviePoster = styled.img`
+  width: 605px;
+  height: 398px;
+  object-fit: cover;
+  max-width: 100%;
+`;
 
 export const MovieTitle = styled.h3``;
 
 export const MovieDescriptionBlock = styled.div``;
 
-export const MovieDescrBlockItem = styled.li``;
+export const MovieDescrBlockItem = styled.li`
+  display: flex;
+  flex-direction: row;
+`;
 
 export const MovieDescriptionTitle = styled.p``;
